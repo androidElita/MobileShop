9e9f4efcee401bdec5e1f48c9d081654c8308b84
gaponov <s.gaponov@e2e4online.ru>
2019-11-26 10:39:42 +0700

    Рефакторинг окна подкатегорий. Добавление анимации заголовка toolbar.

--- app/src/main/java/ru/e2e4/shopmobile/modules/catalog/view/GoodsListFragment.kt
+++ app/src/main/java/ru/e2e4/shopmobile/modules/catalog/view/GoodsListFragment.kt
@@ -1,25 +1,30 @@
 package ru.e2e4.shopmobile.modules.catalog.view
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.main_toolbar.*
 import ru.e2e4.shopmobile.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class GoodsListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.catalog_fragment_goods_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        (activity as AppCompatActivity).setSupportActionBar(vToolbar)
+        vToolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+        vToolbar.setNavigationOnClickListener { onBackPressed() }
+    }
 
+    private fun onBackPressed() {
+        (activity as AppCompatActivity).onBackPressed()
+    }
 }
--- app/src/main/java/ru/e2e4/shopmobile/modules/catalog/view/SubcategoriesFragment.kt
+++ app/src/main/java/ru/e2e4/shopmobile/modules/catalog/view/SubcategoriesFragment.kt
@@ -1,26 +1,23 @@
 package ru.e2e4.shopmobile.modules.catalog.view
 
+import android.animation.ObjectAnimator
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.*
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.catalog_fragment_subcategories.view.*
-import kotlinx.android.synthetic.main.main_toolbar.*
+import kotlinx.android.synthetic.main.catalog_fragment_subcategories.*
+import kotlinx.android.synthetic.main.common_toolbar.*
 import ru.e2e4.shopmobile.R
 import ru.e2e4.shopmobile.modules.catalog.adapters.CategoriesAdapter
-import ru.e2e4.shopmobile.modules.catalog.contract.SubcategoriesContract.SubcategoriesView
 
-class SubcategoriesFragment : Fragment(), SubcategoriesView {
+class SubcategoriesFragment : Fragment() {
 
-    private lateinit var rootView: View
     private var isHideTitleToolbar = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,32 +34,60 @@ class SubcategoriesFragment : Fragment(), SubcategoriesView {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        rootView = view
-        val toolbar = view.vToolbar as Toolbar?
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
-        toolbar?.setNavigationIcon(R.drawable.ic_arrow_back)
-        toolbar?.setNavigationOnClickListener { (activity as AppCompatActivity).onBackPressed() }
+        (activity as AppCompatActivity).setSupportActionBar(vToolbar)
+        vToolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+        vToolbar.setNavigationOnClickListener { onBackPressed() }
 
         val category = SubcategoriesFragmentArgs.fromBundle(arguments!!).category
-        view.vNameCategory.text = category.name
 
-        view.vSubcategories.layoutManager = GridLayoutManager(activity, 3)
-        view.vSubcategories.addItemDecoration(GridItemDecoration())
+        vNameCategory.text = category.name
 
         val adapter = CategoriesAdapter(category.children)
         adapter.onItemClickListener = { subcategory ->
             val action =
-                SubcategoriesFragmentDirections.actionSubcategoriesFragmentSelf(subcategory)
+                if (subcategory.children.isEmpty())
+                    SubcategoriesFragmentDirections.actionSubcategoriesFragmentToGoodsListFragment()
+                else
+                    SubcategoriesFragmentDirections.actionSubcategoriesFragmentSelf(subcategory)
             findNavController().navigate(action)
         }
-        view.vSubcategories.adapter = adapter
 
-        view.vScroll.viewTreeObserver.addOnScrollChangedListener {
-            val scrollY = view.vScroll.scrollY
-            if (scrollY > 100 && isHideTitleToolbar) {
-                animateToolbarTitleHide(category.name, vToolbar)
-            } else if (scrollY < 100 && !isHideTitleToolbar) animateToolbarTitleShow(" ", vToolbar)
+        vSubcategories.layoutManager = GridLayoutManager(activity, 3)
+        vSubcategories.addItemDecoration(ItemDecoration())
+        vSubcategories.adapter = adapter
+
+        vScroll.setOnScrollChangeListener(
+            NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+                if (scrollY >= vNameCategory.height && isHideTitleToolbar)
+                    showToolbarTitle(category.name)
+                else if (scrollY < vNameCategory.height && !isHideTitleToolbar) hideToolbarTitle()
+            }
+        )
+
+    }
+
+    private fun showToolbarTitle(title: String) {
+        val vToolbarTitle = getToolbarTitleView().apply { text = title }
+        ObjectAnimator.ofFloat(vToolbarTitle, View.ALPHA, 0f, 1f)
+            .setDuration(300)
+            .start()
+        isHideTitleToolbar = false
+    }
+
+    private fun hideToolbarTitle() {
+        ObjectAnimator.ofFloat(getToolbarTitleView(), View.ALPHA, 1f, 0f)
+            .setDuration(100)
+            .start()
+        isHideTitleToolbar = true
+    }
+
+    private fun getToolbarTitleView(): TextView {
+        val childCount = vToolbar.childCount
+        repeat(childCount) {
+            val child = vToolbar.getChildAt(it)
+            if (child is TextView) return child
         }
+        return TextView(activity)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -78,50 +103,15 @@ class SubcategoriesFragment : Fragment(), SubcategoriesView {
         return true
     }
 
-    private fun getToolbarTitleView(toolbar: Toolbar): TextView {
-        val childCount = toolbar.childCount
-        repeat(childCount) {
-            val child = toolbar.getChildAt(it)
-            if (child is TextView) return child
-        }
-        return TextView(activity)
+    private fun onBackPressed() {
+        (activity as AppCompatActivity).onBackPressed()
     }
 
-    private fun animateToolbarTitleHide(title: String, toolbar: Toolbar) {
-        val vTitle = getToolbarTitleView(toolbar)
-        vTitle.text = title
-        val animationHide = AnimationUtils.loadAnimation(activity, R.anim.toolbar_title_hide)
-        vTitle.startAnimation(animationHide)
-        isHideTitleToolbar = false
-    }
+    inner class ItemDecoration : RecyclerView.ItemDecoration() {
 
-    private fun animateToolbarTitleShow(title: String, toolbar: Toolbar) {
-        val vTitle = getToolbarTitleView(toolbar)
-        val animationShow = AnimationUtils.loadAnimation(activity, R.anim.toolbar_title_show)
-        vTitle.startAnimation(animationShow)
-        animationShow.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationRepeat(animation: Animation?) {
-
-            }
-
-            override fun onAnimationEnd(animation: Animation?) {
-                vTitle.text = " "
-            }
-
-            override fun onAnimationStart(animation: Animation?) {
-
-            }
-        })
-        isHideTitleToolbar = true
-    }
-
-    inner class GridItemDecoration : RecyclerView.ItemDecoration() {
-
-        private val offset12 = resources.getDimension(R.dimen.value12).toInt()
-        private val offset16 = resources.getDimension(R.dimen.value16).toInt()
-        private val offset3 = resources.getDimension(R.dimen.value3).toInt()
         private val offset2 = resources.getDimension(R.dimen.value2).toInt()
         private val offset9 = resources.getDimension(R.dimen.value9).toInt()
+        private val offset16 = resources.getDimension(R.dimen.value16).toInt()
         private val offset24 = resources.getDimension(R.dimen.value24).toInt()
 
         override fun getItemOffsets(
